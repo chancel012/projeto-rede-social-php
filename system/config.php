ff19bd5403782b570c92294ca72ecd6298ae5575
Chancel12 <89141879+Chancel12@users.noreply.github.com>
2022-07-11 21:42:43 -0300

    main

---
+++ system/config.php
@@ -0,0 +1,9 @@
+<?php 
+define('__DIR_PRINCIPAL__', '/');
+//definido dados do banco de dados
+//define('SQL_DB_SERVER', 'localhost');
+//define('SQL_DB_USER', 'root');
+//define('SQL_DB_PASS', '');
+//define('SAL_DB_DATABASE', 'localredesocial');
+
+?>
